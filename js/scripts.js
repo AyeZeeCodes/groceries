8e5e7957db2a3f22024502816fc8c92c07fa3cbe
Aaron Porter and Alex Zerbach <alex.zerbach@gmail.com>
2016-06-15 19:54:25 -0700

    uppercase and sort grocery list a-z and output to list

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,23 @@ $(document).ready(function() {
   $("#groceries").submit(function(event) {
     event.preventDefault();
 
-    var item = $("input#item").val();
+    var item = $("#item").val();
     groceryList.push(item);
+    $("#item").val("");
+  });
+
+  $("#submitList").click(function(event) {
+    event.preventDefault();
+
+    var finishedList = groceryList.map(function(element) {
+      return element.toUpperCase();
+    });
+
+    finishedList.sort();
+    finishedList.forEach(function(element) {
+      $("#output").append("<li>" + element + "</li>");
+    });
+
+    $("#output").show();
   });
 });
